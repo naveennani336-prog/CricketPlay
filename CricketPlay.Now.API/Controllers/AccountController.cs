3f9aeb59daf76cb8ea6b4c2636366b03ad62eb7b
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-11-22 19:27:41 +0530

    Configuraed API

---
+++ CricketPlay.Now.API/Controllers/AccountController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CricketPlay.Now.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
