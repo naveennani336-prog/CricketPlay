b738a90695b34fa8fc4707812bb0c8f09e7476b3
PUNEM NAVEEN KUMAR <npunem@betalen.in>
2025-11-22 18:57:54 +0530

    Add project files.

---
+++ CricketPlay.Now.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CricketPlay.Now.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
